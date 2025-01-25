5e32031c38677bdd528fcd40af05e3ca82501cd3
Korawich Anuttra <korawich.anu@gmail.com>
2025-01-25 15:19:36 +0700

    :toolbox: build: update pyproject file.

--- clishelf/__main__.py
+++ clishelf/__main__.py
@@ -3,6 +3,6 @@
 # Licensed under the MIT License. See LICENSE in the project root for
 # license information.
 # ------------------------------------------------------------------------------
-from .cli import main
+from clishelf.cli import main
 
 main()
